1e9ecb8c29053c0bb661d18bb802697d1ee93920
Pravin Mishra <diatm.pravin.it.07.27@gmail.com>
2013-01-26 22:57:28 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExportCsvDemo::Application.config.secret_token = '28f8afe30ca656b148eea5cc43b55dc9e790209ec74216e45e47fe5ab0f419c6822cc485e238b41eb23e72e99a40edcbd316ee1b3d13850657433dc28eaf318a'
